a79de91a2ba36ca47302ec4c4ca8cf0880d9d1b6
Utkarsh Saxena <utkarsh.saxena@unicommerce.com>
2024-02-15 20:22:28 +0530

    Add method to find student by ID + Refactor

--- src/main/java/com/btimfl/restDemo/controller/StudentRestController.java
+++ src/main/java/com/btimfl/restDemo/controller/StudentRestController.java
@@ -1,7 +1,9 @@
 package com.btimfl.restDemo.controller;
 
 import com.btimfl.restDemo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,14 +13,25 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
-    @GetMapping("/students")
-    public List<Student> listAllStudents() {
-        List<Student> studentList = new ArrayList<Student>();
+
+    private List<Student> studentList;
+
+    @PostConstruct
+    private void loadData() {
+        studentList = new ArrayList<Student>();
 
         studentList.add(new Student("Sanatvikram", "Bisht"));
         studentList.add(new Student("Tuhin", "Upadhyay"));
         studentList.add(new Student("Sathyam", "Rajpal"));
+    }
 
+    @GetMapping("/students")
+    public List<Student> listAllStudents() {
         return studentList;
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student findStudentById(@PathVariable("studentId") int studentId) {
+        return studentList.get(studentId);
     }
 }
